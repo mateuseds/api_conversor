4c12e9ff902f919a62f6b8e9f3870581cea0663a
Mateus Souza <80061497+mateuseds@users.noreply.github.com>
2022-06-09 17:49:40 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,21 +1,41 @@
+const formConvert = document.getElementById('formConvert');
+
+function onlynumber(evt) {
+    let theEvent = evt || window.event;
+    let key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    let regex = /^[0-9.]+$/;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
+
 document.addEventListener("keypress", function(e) {
     if (e.key === 'Enter') {
-        const buttonDolar = document.querySelector("#converterDolar");
-        buttonDolar.click();
+        const botaoConversor = document.querySelector("#inputConvert");
+        botaoConversor.click();
     }
 });
 
-function converterDolar() {
-    const valorElemento = document.getElementById("dolar");
-    const valor = valorElemento.value;
-    const valorEmDolarNumerico = parseFloat(valor);
-    const valorEmReal = valorEmDolarNumerico * 5;
-    const elementoValorConvertido = document.getElementById("valorConvertido");
-    const valorConvertido = "$" + valor + " = R$" + valorEmReal.toFixed(2);
+const fetchPromise = async() =>
+    await fetch('https://economia.awesomeapi.com.br/last/USD-BRL');
 
-    if (valor == 0) {
-        elementoValorConvertido.innerHTML = "Insira algum valor em dolar";
-    } else {
-        elementoValorConvertido.innerHTML = valorConvertido;
+formConvert.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const dolarInput = document.getElementById('inputDolar');
+    const valorConvertido = document.getElementById('valorConvertido')
+
+    if (!dolarInput.value) {
+        valorConvertido.innerHTML = 'Insira um valor em Dolar';
+        return;
     }
-};
+    const dolarPreenchido = parseFloat(dolarInput.value);
+
+    fetchPromise().then(async(response) => {
+        const { USDBRL } = await response.json();
+        console.log(USDBRL)
+        const resultado = dolarPreenchido * USDBRL.high;
+        valorConvertido.innerHTML = `R$ ${resultado.toFixed(2)}`;
+    });
+});
