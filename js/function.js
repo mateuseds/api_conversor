18a3c0959a1ce6a9013f0faea8e8c9a0e175746e
Mateus Souza <80061497+mateuseds@users.noreply.github.com>
2022-06-12 22:00:13 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,16 +1,5 @@
 const formConvert = document.getElementById('formConvert');
 
-function onlynumber(evt) {
-    let theEvent = evt || window.event;
-    let key = theEvent.keyCode || theEvent.which;
-    key = String.fromCharCode(key);
-    let regex = /^[0-9.]+$/;
-    if (!regex.test(key)) {
-        theEvent.returnValue = false;
-        if (theEvent.preventDefault) theEvent.preventDefault();
-    }
-}
-
 document.addEventListener("keypress", function(e) {
     if (e.key === 'Enter') {
         const botaoConversor = document.querySelector("#inputConvert");
@@ -34,7 +23,6 @@ formConvert.addEventListener('submit', (e) => {
 
     fetchPromise().then(async(response) => {
         const { USDBRL } = await response.json();
-        console.log(USDBRL)
         const resultado = dolarPreenchido * USDBRL.high;
         valorConvertido.innerHTML = `R$ ${resultado.toFixed(2)}`;
     });
